adb120193a1c5562915456b9ec5288313f23674e
BeachAndBytes <beachandbytes@gmail.com>
2013-09-19 09:21:50 -1000

    Initial Commit

---
+++ ToggleLineNumbers/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace BeachAndBytes.ToggleLineNumbers
+{
+    static class GuidList
+    {
+        public const string guidToggleLineNumbersPkgString = "0d51db24-b042-4403-b5dc-97d193ca0a45";
+        public const string guidToggleLineNumbersCmdSetString = "3299446f-eb0f-4b28-b106-b2e231243930";
+
+        public static readonly Guid guidToggleLineNumbersCmdSet = new Guid(guidToggleLineNumbersCmdSetString);
+    };
+}
---
+++ ToggleLineNumbers/ToggleLineNumbersPackage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using BeachAndBytes.ToggleLineNumbers;
+using Microsoft.VisualStudio.Shell;
+using EnvDTE;
+using EnvDTE80;
+
+namespace StudioPackages.ToggleLineNumbers
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(GuidList.guidToggleLineNumbersPkgString)]
+    public sealed class ToggleLineNumbers : Package
+    {
+        private DTE2 _application;
+        private bool _lineNumbersVisible;
+        private List<string> _editorTypes;
+
+        protected override void Initialize()
+        {
+            _application = (DTE2)GetService(typeof(DTE));
+            _lineNumbersVisible = (bool)_application.Properties[@"TextEditor", "Basic"].Item(@"ShowLineNumbers").Value;
+            _editorTypes = new List<string> { "PlainText", "CSharp", "HTML", "C/C++", "XML", "CSS", "Basic", "CSS", "FSharp", "Javascript", "Xaml" };
+            base.Initialize();
+
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null == mcs) return;
+            mcs.AddCommand(new MenuCommand(MenuItemCallback, new CommandID(GuidList.guidToggleLineNumbersCmdSet, (int)PkgCmdIDList.cmdidToggleLineNumbers)));
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            foreach (string editor in _editorTypes)
+            {
+                _application.Properties[@"TextEditor", editor].Item(@"ShowLineNumbers").Value = !_lineNumbersVisible;
+            }
+            _lineNumbersVisible = !_lineNumbersVisible;
+        }
+    }
+}
